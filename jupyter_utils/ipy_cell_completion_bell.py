0faafda888ace6fd37659aad889b3dce4d96cb9c
Nathan Hunt <neighthan.hunt@gmail.com>
2020-03-31 16:55:27 -0400

    Update %notify to use a local file, if available.

--- jupyter_utils/ipy_cell_completion_bell.py
+++ jupyter_utils/ipy_cell_completion_bell.py
@@ -26,11 +26,12 @@ command2()
 To always play your preferred audio file, just change the default below.
 """
 
-from IPython.display import Audio, display
+from pathlib import Path
+from typing import Optional
+from IPython import get_ipython
 from IPython.core.magic import line_cell_magic, Magics, magics_class
 from IPython.core.magic_arguments import argument, magic_arguments, parse_argstring
-from IPython import get_ipython
-from typing import Optional
+from IPython.display import Audio, display
 
 
 class _InvisibleAudio(Audio):
@@ -51,14 +52,18 @@ class _InvisibleAudio(Audio):
 class NotificationMagics(Magics):
     """
     Inspired by https://stackoverflow.com/a/50648266.
+
+    If a bell.wav file exists in the same directory as this file, that will be used as
+    the default sound. Otherwise, the default is the wav file at `DEFAULT_URL`.
+    Providing a URL to the magic overrides either of these defaults.
     """
+
+    SOUND_FILE = str(Path(__file__).parent.resolve() / "bell.wav")
+    DEFAULT_URL = "https://freewavesamples.com/files/E-Mu-Proteus-FX-CosmoBel-C3.wav"
 
     @magic_arguments()
     @argument(
-        "-u",
-        "--url",
-        default="https://freewavesamples.com/files/E-Mu-Proteus-FX-CosmoBel-C3.wav",
-        help="URL of audio file to play.",
+        "-u", "--url", default=SOUND_FILE, help="URL of audio file to play.",
     )
     @argument(
         "line_code",
@@ -73,7 +78,14 @@ class NotificationMagics(Magics):
         try:
             ret = self.shell.ex(code)
         finally:
-            audio = _InvisibleAudio(url=args.url, autoplay=True)
+            maybe_url = args.url
+            if maybe_url == self.SOUND_FILE:
+                if Path(self.SOUND_FILE).is_file():
+                    audio = _InvisibleAudio(filename=maybe_url, autoplay=True)
+                else:
+                    audio = _InvisibleAudio(url=self.DEFAULT_URL, autoplay=True)
+            else:
+                audio = _InvisibleAudio(url=maybe_url, autoplay=True)
             display(audio)
 
         return ret
