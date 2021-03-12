4adb8448e1553ab29b9c1106582a3e6914a40240
Nathan Hunt <neighthan.hunt@gmail.com>
2021-03-12 15:27:05 -0500

    Don't throw error if missing imports in background.

--- jupyter_utils/background.py
+++ jupyter_utils/background.py
@@ -12,51 +12,55 @@ from time import sleep
 
 from IPython.core.magic import register_cell_magic
 
-from jupyter_utils.jupyter_utils import get_notebook_path
+try:
+    from jupyter_utils.jupyter_utils import get_notebook_path
+    imports_ok = True
+except ImportError:
+    imports_ok = False
 
+if imports_ok:
+    @register_cell_magic
+    def background(line: str, cell: str):
+        """
+        Any cells with cell magics will be skipped (in case it's something like %%bash) as
+        will any lines with line magics. The exception is that the rest of the code in the
+        cell that's running this magic will be included.
+        """
+        nb_fname = get_notebook_path()
+        if nb_fname is None:
+            raise RuntimeError("Unable to find path to current notebook.")
 
-@register_cell_magic
-def background(line: str, cell: str):
-    """
-    Any cells with cell magics will be skipped (in case it's something like %%bash) as
-    will any lines with line magics. The exception is that the rest of the code in the
-    cell that's running this magic will be included.
-    """
-    nb_fname = get_notebook_path()
-    if nb_fname is None:
-        raise RuntimeError("Unable to find path to current notebook.")
+        with open(nb_fname) as f:
+            nb = json.load(f)
 
-    with open(nb_fname) as f:
-        nb = json.load(f)
+        code = ""
 
-    code = ""
-
-    terminate = False
-    for cell in nb["cells"]:
-        # skip markdown and empty code cells
-        if cell["cell_type"] != "code" or not cell["source"]:
-            continue
-
-        if cell["source"][0].startswith("%%"):
-            if cell["source"][0].startswith("%%background"):  # end after this cell
-                cell["source"] = cell["source"][1:]  # skip the line with the magic
-                terminate = True
-            else:  # skip cells with any other cell magics; this might, e.g., be something like %%bash
+        terminate = False
+        for cell in nb["cells"]:
+            # skip markdown and empty code cells
+            if cell["cell_type"] != "code" or not cell["source"]:
                 continue
 
-        # skip line magics
-        code += (
-            "".join([line for line in cell["source"] if not line.startswith("%")])
-            + "\n"
-        )
-        if terminate:
-            break
+            if cell["source"][0].startswith("%%"):
+                if cell["source"][0].startswith("%%background"):  # end after this cell
+                    cell["source"] = cell["source"][1:]  # skip the line with the magic
+                    terminate = True
+                else:  # skip cells with any other cell magics; this might, e.g., be something like %%bash
+                    continue
 
-    # execute code, from the same directory in case relative paths are used
-    with NamedTemporaryFile("w", dir=dirname(nb_fname)) as tmp:
-        tmp.write(code)
-        tmp.seek(0)  # running cat didn't show anything unless I did this
+            # skip line magics
+            code += (
+                "".join([line for line in cell["source"] if not line.startswith("%")])
+                + "\n"
+            )
+            if terminate:
+                break
 
-        Popen(["python", tmp.name])
-        # otherwise, it seems like the file is deleted before Popen even gets to it
-        sleep(0.5)
+        # execute code, from the same directory in case relative paths are used
+        with NamedTemporaryFile("w", dir=dirname(nb_fname)) as tmp:
+            tmp.write(code)
+            tmp.seek(0)  # running cat didn't show anything unless I did this
+
+            Popen(["python", tmp.name])
+            # otherwise, it seems like the file is deleted before Popen even gets to it
+            sleep(0.5)
